b6d2d4e327d1ca0e55de6f4a276193cc2529bdd3
emilyyleung <30677901+emilyyleung@users.noreply.github.com>
2022-08-27 06:22:50 +0000

    Deploying to gh-pages from @ emilyyleung/emilyyleung.github.io@f26f49f2be37b12c3ed8a33a12d3113849163791 🚀

---
+++ _next/static/e00seX6LawCUxQC2sKAV5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3df14607660072fb.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
